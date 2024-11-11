4dbaa1562e0fccab1bce561e7dc2c580f2b93fb8
ADEDAMOLA <adesamueldamola@gmail.com>
2024-11-11 01:46:13 -0800

    first

---
+++ lib/onboarding.dart/onboading.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/materials.dart';
+
+
+class Onboading extends StatefulWidget {
+  const Onboarding({super.key});
+  @override
+  State<Onboarding> createState() =>OnboardingState();
+}
+
+class OnboardingState extends StatelessWidget {
+  const MyFlutterApp
+}
